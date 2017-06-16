c83484d10a3006ed935886cf535d852fbee2c2b7
YT <yongtao.fan@yunshipei.com>
2017-06-16 10:49:28 +0800

    add stardandjs

--- index.js
+++ index.js
@@ -1,137 +1,137 @@
 /*!
- * yky
+ * knife-json
  * Copyright(c) 2017 yt_fan
  * MIT Licensed
  */
-'use strict';
-var toStr = Object.prototype.toString;
+'use strict'
+
+var toStr = Object.prototype.toString
 
 /**
  * @private
- * @param {Obj} obj 
+ * @param {Obj} obj
  * @returns {Boolen}
  */
-function isObject(obj) {
-  return toStr.call(obj) === '[object Object]';
+function isObject (obj) {
+  return toStr.call(obj) === '[object Object]'
 }
 
 /**
  * @private
- * @param {Obj} obj 
+ * @param {Obj} obj
  * @returns {Boolen}
  */
-function isString(obj) {
-  return toStr.call(obj) === '[object String]';
+function isString (obj) {
+  return toStr.call(obj) === '[object String]'
 }
 
-/**!
+/** !
  * @private
  * @param {Object} object
- * @param {String} parent 
+ * @param {String} parent
  * @returns {Array}
  */
-function formatPath(obj, parent) {
-  parent = parent || '';
-  var array = [];
-  for(var key in obj) {
-    if(isObject(obj[key])) {
-      parent = parent + '.' + key;
+function formatPath (obj, parent) {
+  parent = parent || ''
+  var array = []
+  for (var key in obj) {
+    if (isObject(obj[key])) {
+      parent = parent + '.' + key
       var tmp = formatPath(obj[key], parent)
-      array = array.concat(tmp);
+      array = array.concat(tmp)
     } else {
-      array.push(parent + '.' + key);
+      array.push(parent + '.' + key)
     }
   }
-  return array;
+  return array
 }
 
 /*
- * @param {any} object 
- * @param {any} array 
- * @returns 
+ * @param {any} object
+ * @param {any} array
+ * @returns
  */
-function _paths(object, array) {
-  array = Array.isArray(array) ? array : [];
-  for(var key in object) {
-    if(isObject(object[key])) {
-      var tmp = formatPath(object[key], key);
-      array = array.concat(tmp);
+function _paths (object, array) {
+  array = Array.isArray(array) ? array : []
+  for (var key in object) {
+    if (isObject(object[key])) {
+      var tmp = formatPath(object[key], key)
+      array = array.concat(tmp)
     } else {
-      array.push(key);
+      array.push(key)
     }
   }
-  return array;
+  return array
 }
 
 /**
  * @private
- * @param {any} obj 
- * @param {any} path 
- * @returns 
+ * @param {any} obj
+ * @param {any} path
+ * @returns
  */
-function _get(obj, path) {
-  var paths = path.split('.');
-  for(var i=0; i< paths.length; i++) {
-    if(isObject(obj[paths[i]]) && paths[i+1]) {
-      path = paths.slice(1).join('.');
-      return _get(obj[paths[i]], path);
+function _get (obj, path) {
+  var paths = path.split('.')
+  for (var i = 0; i < paths.length; i++) {
+    if (isObject(obj[paths[i]]) && paths[i + 1]) {
+      path = paths.slice(1).join('.')
+      return _get(obj[paths[i]], path)
     } else {
-      return obj[paths[i]];
+      return obj[paths[i]]
     }
   }
 }
 
 /**
  * @private
- * @param {Object} obj 
- * @param {String} path 
+ * @param {Object} obj
+ * @param {String} path
  * @returns {Boolen}
  */
-function _contain(obj, path) {
-  var paths = path.split('.');
-  for(var i=0; i< paths.length; i++) {
-    if(isObject(obj[paths[i]]) && paths[i+1]) {
-      path = paths.slice(1).join('.');
-      return _get(obj[paths[i]], path);
+function _contain (obj, path) {
+  var paths = path.split('.')
+  for (var i = 0; i < paths.length; i++) {
+    if (isObject(obj[paths[i]]) && paths[i + 1]) {
+      path = paths.slice(1).join('.')
+      return _get(obj[paths[i]], path)
     } else {
-      return !!obj[paths[i]];
+      return !!obj[paths[i]]
     }
   }
 }
 
-/**!
+/** !
  * @public
  * @param {Object} object
  * @param {String} path
  * @return {Any}
  */
-module.exports = function(obj, path) {
-  if(!isString(path) || !isObject(obj)) 
-    throw new Error('Need a object and a path, please!!!')
-  path = path || path.trim();
-  return _get(obj, path);
-};
+module.exports = function (obj, path) {
+  if (!isString(path) || !isObject(obj)) { throw new Error('Need a object and a path, please!!!') }
+  path = path || path.trim()
+  return _get(obj, path)
+}
 
 /**
  * @public
  * @param {Object} object
  * @return {Array}
  */
-module.exports.paths = function(object) {
-  if(arguments.length < 1) throw new Error('Need a object, please!!!');
-  var array = [];
-  if(!isObject(object)) return array;
-  return _paths(object, array);
-};
+module.exports.paths = function (object) {
+  if (arguments.length < 1) throw new Error('Need a object, please!!!')
+  var array = []
+  if (!isObject(object)) return array
+  return _paths(object, array)
+}
 
 /**
- * 
+ *
  * @public contain
  * @param {Object} object
  * @param {String} path
  * @return {Boolen}
  */
-module.exports.contain = function(object, path) {
-  if(!isObject(object) || !isString(path)) return false;
-  return _contain(object, path);
-};
+module.exports.contain = function (object, path) {
+  if (!isObject(object) || !isString(path)) return false
+  return _contain(object, path)
+}
--- test.js
+++ test.js
@@ -1,28 +1,28 @@
-'use strict';
-var test = require('ava').test;
-var knife = require('./index');
+'use strict'
+var test = require('ava').test
+var knife = require('./index')
 var testdata = {
   root: 'root',
   name: 'yt',
   nested: {
     nested1: {
-      nested2: "nested3"
+      nested2: 'nested3'
     }
   }
-};
+}
 
-test('#Test get value method', function(t) {
-  t.pass(knife(testdata, 'root'), 'root');
-  t.pass(knife(testdata, 'name'), 'yt');
-  t.pass(knife(testdata, 'nested.nested1.nested2'), 'nested3');
-});
+test('#Test get value method', function (t) {
+  t.pass(knife(testdata, 'root'), 'root')
+  t.pass(knife(testdata, 'name'), 'yt')
+  t.pass(knife(testdata, 'nested.nested1.nested2'), 'nested3')
+})
 
-test('#Test contain method', function(t) {
-  t.pass(knife.contain(testdata, 'nested.nested1'));
-});
+test('#Test contain method', function (t) {
+  t.pass(knife.contain(testdata, 'nested.nested1'))
+})
 
-test('# Get keys of object', function(t) {
-  var keys = knife.paths(testdata);
-  console.log(keys);
-  t.pass(Array.isArray(keys));
-});
+test('# Get keys of object', function (t) {
+  var keys = knife.paths(testdata)
+  console.log(keys)
+  t.pass(Array.isArray(keys))
+})
